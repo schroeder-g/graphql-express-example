0918d07d924bd316f2c790dc5b3c785b82dce741
Alex Goncalves <schroedergoncalves@gmail.com>
2021-09-28 20:53:17 -0400

    successfully resolving get/all for books, authors, books' author, and authors' books.

--- schema/schema.js
+++ schema/schema.js
@@ -1,26 +1,70 @@
-/* All GraphQL APIs require the schema description. */
+/* All GraphQL APIs require the schema description. 
+    The Schema provides a nice parallelism for 
+
+*/
 const graphql = require('graphql');
 const _ = require('lodash');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; 
+const shID = require('shortid')
+const { 
+    GraphQLSchema,
+    GraphQLObjectType, 
+    GraphQLString,
+    GraphQLInt, 
+    GraphQLID,
+    GraphQLList,
+} = graphql; 
 
 //dummy
 let books = [
-    {name: 'Big Women', genre: 'sci-fi', id: "1"},
-    {name: 'The Libertarian Ideal', genre: 'sci-fi', id: "2"},
-    {name: 'My Life', genre: 'real', id: "3"}
+    {title: 'The Union', genre: 'sci-fi', authorId: "1", id: "1"},
+    {title: 'The Libertarian Ideal', genre: 'sci-fi', authorId: "2", id: "2" },
+    {title: 'To Fields on Io', genre: 'biography', authorId: "3",  id: "3"},
+    {title: "The Winter of Our Discontent", genre: 'fiction', authorId: 5 ,  id: "4"},
+    {title: "Swann's Way", genre: 'fiction', authorId: "4",  id: "5"},
+    {title: "Grapes of Wrath", genre: 'fiction', authorId: "5",  id: "6"}
+
 ]
+
+let authors = [
+    {name:'Donald Mikado', age:'26', id: "1", books: {}}, //IDs don't have to (but probably should) share types
+    {name:'Samuel P. Clemens', age:'100', id: "2"},
+    {name:'Reb Monk', age:'100', id: "3", books:{ }},
+    {name:'Marcel Proust', age:'980', id: "4", books:{ }},
+    {name:'John Steinbeck', age:'98', id: "5", books:{ }}
+]
+
+/* Strict typing allows us to overcome 
+multiple type ref errors */
+const AuthorType = new GraphQLObjectType({ 
+    name: 'Author',
+    fields: () => ({ // A function to handle asynchronicity (allowing all entities to resolve first)
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: { type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, { authorId : parent.id })
+            }
+        }
+    })
+})
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: () => ({
-    /* A function is needed bc when we have multiple
-       inter-referencing fields, one field may not now
-       about another field's type */
-       id: {type: GraphQLString }, 
-       name: { type: GraphQLString },
-       genre: { type: GraphQLString }
-    /* Strict typing allows us to overcome 
-       multiple type ref errors */
+    fields: () => ({ 
+    /* An obj method is needed to handle inter-referencing fields, and mixed */
+       id: {type: GraphQLID }, //GQLID purely for GQL benefit; can be string
+       title: { type: GraphQLString },
+       genre: { type: GraphQLString },
+       author: {
+           type: AuthorType,
+           resolve(parent, args){
+               console.log(parent)
+               return _.find(authors, {id: parent.authorId})
+           } // GraphQLFieldResolver(source: any, args: { [argName: string]: any; },
+       }
+    /* */
     })
 })
 
@@ -32,16 +76,32 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-        /*  Args are required parameters for query.
-            Must be consistent with previously defined fields. */
-            args: {id: {type: GraphQLString}}, 
-        /*  Resolve function defines logic for data retrieval.
-            Grabs from db or other source. */
+            args: {id: {type: GraphQLID}}, 
             resolve(parent, args){
                 return _.find(books, {id: args.id});
             }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}}, 
+            resolve(parent, args){ 
+                return _.find(authors, {id: args.id})
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
+            }
         }
-     }
+
+    }
  })
 
 module.exports = new GraphQLSchema({
